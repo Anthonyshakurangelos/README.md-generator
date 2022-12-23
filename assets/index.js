60d0149997aada20610424b509c9447a0251005e
Anthonyshakurangelos <anthonyshakurangelos@gmail.com>
2022-12-22 17:22:28 -0700

    first commit

---
+++ assets/index.js
@@ -0,0 +1,82 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs')
+// TODO: Create an array of questions for user input
+// const questions = [];
+
+// TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+function init() {
+
+const generateREADMEmd = ({ title, description, motivation, installation, usage, credits, license }) =>
+
+```md
+# ${title}
+
+## Description
+${description}
+
+## 
+ ${installation}
+
+## Usage
+${usage}
+
+## Credits 
+${credits}
+
+## License
+${license}
+```;
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is your title name?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'what is your description of this app?',
+    },
+    {
+      type: 'input',
+      name: 'motivation',
+      message: 'What is your motivation?',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'What are the steps required to install your project?',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'usage? Example..([alt text(assets/images/screenshot.png)]',
+    },
+    {
+      type: 'input',
+      name: 'credits',
+      message: 'List your collaborators, if any, with links with their GitHub',
+    },
+    {
+      type: 'input',
+      name: 'License',
+      message: 'Add license',
+    },
+  ])
+  .then((answers) => {
+    const READMEmdFile = generateREADMEmd(answers);
+
+    fs.writeFile('README.md', READMEmdFile, (err) =>
+      err ? console.log(err) : console.log('Successfully created README.md!')
+    );
+  });
+// TODO: Create a function to initialize app
+ }
+
+// Function call to initialize app
+init();
